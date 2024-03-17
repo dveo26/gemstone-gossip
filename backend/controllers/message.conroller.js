e6e541f33420e08a2b8ca659b3e1b70473df4301
AmanSingh9918 <aman.20227008@mnnit.ac.in>
2024-03-18 04:46:34 +0530

    implemented socket.io and online indicator

--- backend/controllers/message.conroller.js
+++ backend/controllers/message.conroller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -26,13 +27,17 @@ export const sendMessage = async (req, res) => {
     if (newMessage) {
       conversation.messages.push(newMessage._id);
     }
-
-    //socket.io functionality will go here to make it real time
-
     //await conversation.save();
     //await newMessage.save();
     //this will run in parallel
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    //socket.io functionality will go here to make it real time
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      //to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,9 @@ import userRoutes from "./routes/user.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import cookieParser from "cookie-parser";
 import connectToMongoDB from "./db/connectToMongooseDB.js";
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
-const app = express();
 
 dotenv.config();
 
@@ -24,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);
 });
